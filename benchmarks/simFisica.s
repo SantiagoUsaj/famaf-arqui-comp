4d06afcb00daf606ec41524b6dad75feef6756ec
Santiago Usaj <santiago.usaj@mi.unc.edu.ar>
2024-11-17 19:17:41 -0300

    una idea que funciona pero tarda mucho

--- benchmarks/simFisica.s
+++ benchmarks/simFisica.s
@@ -2,10 +2,14 @@
 	N:       .dword 64	
 	t_amb:   .dword 25   
 	n_iter:  .dword 10    
+	fc_x:   .dword 32                    // Fila central
+	fc_y:   .dword 32                    // Columna central
+	fc_temp:.double 100.0                // Temperatura del centro
+	four:   .double 4.0                  // Constante para división
 	
 	.bss 
 	x: .zero  3072        
-	x_temp: .zero  3072    
+	x_tmp: .zero  3072    
 
 	.arch armv8-a
 	.text
@@ -24,15 +28,163 @@ main:
 	mov	x0, 0
 	bl	m5_dump_stats
 
-	ldr     x0, N
-    ldr     x1, =x 
-    ldr     x2, =x_temp
-    ldr     x3, n_iter
-	ldr     x4, t_amb
-
 //---------------------- CODE HERE ------------------------------------
 
+    // Inicializar la matriz
+    ldr     x0, =N                  // Tamaño de la matriz (64)
+    ldr     x0, [x0]                // Cargar el valor de N
+    ldr     x1, =x                  // Dirección de x
+    ldr     x2, =x_tmp              // Dirección de x_tmp
+    ldr     x3, =n_iter             // Dirección de n_iter
+    ldr     x3, [x3]                // Cargar el valor de n_iter
+    ldr     x4, =t_amb              // Dirección de t_amb
+    ldr     d1, [x4]                // Cargar t_amb en d1
+    mov     x5, #0                  // i = 0
+    mul     x6, x0, x0              // N*N
 
+/* initialize_loop:
+    cmp     x5, x6                  // Comparar i con N*N
+    bge     end_initialize          // Salir si i >= N*N
+    str     d1, [x1, x5, lsl #3]    // Almacenar t_amb en x[i] (8 bytes por celda)
+    add     x5, x5, #1              // Incrementar i
+    b       initialize_loop         // Repetir */
+
+end_initialize:
+    // Establecer x[fc_x*N + fc_y] = fc_temp
+    ldr     x16, =fc_x              // Dirección de fc_x
+    ldr     x16, [x16]              // Cargar fc_x en x16
+    ldr     x17, =fc_y              // Dirección de fc_y
+    ldr     x17, [x17]              // Cargar fc_y en x17
+    ldr     x18, =N                 // Dirección de N
+    ldr     x18, [x18]              // Cargar N en x18
+    mul     x16, x16, x18           // fc_x * N
+    add     x16, x16, x17           // fc_x * N + fc_y
+    ldr     x19, =fc_temp           // Dirección de fc_temp
+    ldr     d5, [x19]               // Cargar fc_temp en d5
+    ldr     x20, =x                 // Dirección de x
+    str     d5, [x20, x16, lsl #3]  // Almacenar fc_temp en x[fc_x * N + fc_y]
+
+iterate_matrix:
+    mov     x7, #0                  // k = 0
+
+outer_loop:
+    cmp     x7, x3                  // Comparar k con n_iter
+    bge     end_iterate             // Salir si k >= n_iter
+    mov     x8, #0                  // i = 0
+
+row_loop:
+    cmp     x8, x0                  // Comparar i con N
+    bge     next_iteration          // Salir si i >= N
+    mov     x9, #0                  // j = 0
+
+column_loop:
+    cmp     x9, x0                  // Comparar j con N
+    bge     next_row                // Salir si j >= N
+    mul     x10, x8, x0             // i * N
+    add     x10, x10, x9            // i * N + j
+    mul     x11, x16, x0            // fc_x * N
+    add     x11, x11, x17           // fc_x * N + fc_y
+    cmp     x10, x11                // Comparar (i*N + j) con (fc_x*N + fc_y)
+    beq     skip_cell               // Saltar si son iguales
+
+    // Calcular la suma de los vecinos
+    fmov    d2, d0                  // sum = 0
+    add     x12, x8, #1             // i + 1
+    cmp     x12, x0                 // Comparar i + 1 con N
+    bge     add_t_amb1              // Si i + 1 >= N, usar t_amb
+    mul     x13, x12, x0            // (i + 1) * N
+    add     x13, x13, x9            // (i + 1) * N + j
+    ldr     d3, [x1, x13, lsl #3]   // Cargar x[(i + 1)*N + j]
+    b       add_sum1
+
+add_t_amb1:
+    ldr     d3, [x4]                // Cargar t_amb en d3
+
+add_sum1:
+    fadd    d2, d2, d3              // sum = sum + x[(i + 1)*N + j] o t_amb
+
+    sub     x12, x8, #1             // i - 1
+    cmp     x12, #0                 // Comparar i - 1 con 0
+    blt     add_t_amb2              // Si i - 1 < 0, usar t_amb
+    mul     x13, x12, x0            // (i - 1) * N
+    add     x13, x13, x9            // (i - 1) * N + j
+    ldr     d3, [x1, x13, lsl #3]   // Cargar x[(i - 1)*N + j]
+    b       add_sum2
+
+add_t_amb2:
+    ldr     d3, [x4]                // Cargar t_amb en d3
+
+add_sum2:
+    fadd    d2, d2, d3              // sum = sum + x[(i - 1)*N + j] o t_amb
+
+    add     x12, x9, #1             // j + 1
+    cmp     x12, x0                 // Comparar j + 1 con N
+    bge     add_t_amb3              // Si j + 1 >= N, usar t_amb
+    mul     x13, x8, x0             // i * N
+    add     x13, x13, x12           // i * N + (j + 1)
+    ldr     d3, [x1, x13, lsl #3]   // Cargar x[i*N + j + 1]
+    b       add_sum3
+
+add_t_amb3:
+    ldr     d3, [x4]                // Cargar t_amb en d3
+
+add_sum3:
+    fadd    d2, d2, d3              // sum = sum + x[i*N + j + 1] o t_amb
+
+    sub     x12, x9, #1             // j - 1
+    cmp     x12, #0                 // Comparar j - 1 con 0
+    blt     add_t_amb4              // Si j - 1 < 0, usar t_amb
+    mul     x13, x8, x0             // i * N
+    add     x13, x13, x12           // i * N + (j - 1)
+    ldr     d3, [x1, x13, lsl #3]   // Cargar x[i*N + j - 1]
+    b       add_sum4
+
+add_t_amb4:
+    ldr     d3, [x4]                // Cargar t_amb en d3
+
+add_sum4:
+    fadd    d2, d2, d3              // sum = sum + x[i*N + j - 1] o t_amb
+
+    // Dividir la suma por 4 y almacenar en x_tmp
+    ldr     x14, =x_tmp             // Dirección de x_tmp
+    ldr     x15, =four              // Dirección de la constante 4.0
+    ldr     d4, [x15]               // Cargar 4.0 en d4
+    fdiv    d2, d2, d4              // sum / 4
+    str     d2, [x14, x10, lsl #3]  // Almacenar sum / 4 en x_tmp[i*N + j]
+
+skip_cell:
+    add     x9, x9, #1              // Incrementar j
+    b       column_loop             // Repetir para la siguiente columna
+
+next_row:
+    add     x8, x8, #1              // Incrementar i
+    b       row_loop                // Repetir para la siguiente fila
+
+next_iteration:
+    ldr     x1, =x                  // Dirección de x
+    ldr     x14, =x_tmp             // Dirección de x_tmp
+    mov     x15, #0                 // i = 0
+
+copy_loop:
+    cmp     x15, x6                 // Comparar i con N*N
+    bge     end_copy                // Salir si i >= N*N
+    mul     x10, x8, x0             // fc_x * N
+    add     x10, x10, x9            // fc_x * N + fc_y
+    cmp     x15, x10                // Comparar i con fc_x*N + fc_y
+    beq     skip_copy               // Saltar si son iguales
+    ldr     d2, [x14, x15, lsl #3]  // Cargar x_tmp[i]
+    str     d2, [x1, x15, lsl #3]   // Almacenar en x[i]
+
+skip_copy:
+    add     x15, x15, #1            // Incrementar i
+    b       copy_loop               // Repetir
+
+end_copy:
+    add     x7, x7, #1              // Incrementar k
+    b       outer_loop              // Repetir para la siguiente iteración
+
+end_iterate:  
+    
 
 //---------------------- END CODE -------------------------------------
 
--- template_test_qemu/main.s
+++ template_test_qemu/main.s
@@ -1,177 +1,180 @@
 .data
-N:      .word 64                    // Tamaño de la matriz
-n_iter: .word 10                    // Número de iteraciones
-fc_x:   .word 32                    // Fila central
-fc_y:   .word 32                    // Columna central
+N:      .dword 2                   // Tamaño de la matriz
+n_iter: .dword 1                    // Número de iteraciones
+fc_x:   .dword 32                   // Fila central
+fc_y:   .dword 32                   // Columna central
 fc_temp:.double 100.0               // Temperatura del centro
-t_amb:  .double 20.0                // Temperatura ambiente
+t_amb:  .double 25.0                // Temperatura ambiente
 x:      .space 32768                // Matriz x (64x64 = 4096 elementos, cada uno de 8 bytes)
 x_tmp:  .space 32768                // Matriz temporal (igual tamaño que x)
 zero:   .double 0.0                 // Constante para inicialización
 four:   .double 4.0                 // Constante para división
 
-    .text
-    .global _start
+.text
+.global _start
 
 _start:
     // Habilitar el coprocesador flotante (FPU)
-	mrs     x1, cpacr_el1
-	mov     x0, #(3 << 20)
-	orr     x0, x1, x0
-	msr     cpacr_el1, x0
+    mrs     x1, cpacr_el1
+    mov     x0, #(3 << 20)
+    orr     x0, x1, x0
+    msr     cpacr_el1, x0
+
     // Inicializar la matriz
-    LDR     X0, =N                  // Tamaño de la matriz (64)
-    LDR     X1, =t_amb              // Dirección de t_amb
-    LDR     D1, [X1]                // Cargar t_amb en D1
-    LDR     X2, =x                  // Dirección de x
-    MOV     X3, #0                  // i = 0
-    MUL     X4, X0, X0              // N*N
+    ldr     x0, =N                  // Tamaño de la matriz (64)
+    ldr     x0, [x0]                // Cargar el valor de N
+    ldr     x1, =x                  // Dirección de x
+    ldr     x2, =x_tmp              // Dirección de x_tmp
+    ldr     x3, =n_iter             // Dirección de n_iter
+    ldr     x3, [x3]                // Cargar el valor de n_iter
+    ldr     x4, =t_amb              // Dirección de t_amb
+    ldr     d1, [x4]                // Cargar t_amb en d1
+    mov     x5, #0                  // i = 0
+    mul     x6, x0, x0              // N*N
 
 initialize_loop:
-    CMP     X3, X4                  // Comparar i con N*N
-    BGE     end_initialize          // Salir si i >= N*N
-    STR     D1, [X2, X3, LSL #3]    // Almacenar t_amb en x[i] (8 bytes por celda)
-    ADD     X3, X3, #1              // Incrementar i
-    B       initialize_loop         // Repetir
+    cmp     x5, x6                  // Comparar i con N*N
+    bge     end_initialize          // Salir si i >= N*N
+    str     d1, [x1, x5, lsl #3]    // Almacenar t_amb en x[i] (8 bytes por celda)
+    add     x5, x5, #1              // Incrementar i
+    b       initialize_loop         // Repetir
 
 end_initialize:
     // Establecer x[fc_x*N + fc_y] = fc_temp
-    LDR     X0, =fc_x               // Dirección de fc_x
-    LDR     X1, [X0]                // Cargar fc_x en X1
-    LDR     X0, =fc_y               // Dirección de fc_y
-    LDR     X2, [X0]                // Cargar fc_y en X2
-    LDR     X3, =N                  // Tamaño de la matriz
-    MUL     X1, X1, X3              // fc_x * N
-    ADD     X1, X1, X2              // fc_x * N + fc_y
-    LDR     X4, =fc_temp            // Dirección de fc_temp
-    LDR     D0, [X4]                // Cargar fc_temp en D0
-    LDR     X5, =x                  // Dirección de x
-    STR     D0, [X5, X1, LSL #3]    // Almacenar fc_temp en x[fc_x * N + fc_y]
+    ldr     x16, =fc_x              // Dirección de fc_x
+    ldr     x16, [x16]              // Cargar fc_x en x16
+    ldr     x17, =fc_y              // Dirección de fc_y
+    ldr     x17, [x17]              // Cargar fc_y en x17
+    ldr     x18, =N                 // Dirección de N
+    ldr     x18, [x18]              // Cargar N en x18
+    mul     x16, x16, x18           // fc_x * N
+    add     x16, x16, x17           // fc_x * N + fc_y
+    ldr     x19, =fc_temp           // Dirección de fc_temp
+    ldr     d5, [x19]               // Cargar fc_temp en d5
+    ldr     x20, =x                 // Dirección de x
+    str     d5, [x20, x16, lsl #3]  // Almacenar fc_temp en x[fc_x * N + fc_y]
 
 iterate_matrix:
-    LDR     X6, =n_iter             // Dirección de n_iter
-    LDR     X6, [X6]                // Cargar n_iter en X6
-    MOV     X7, #0                  // k = 0
+    mov     x7, #0                  // k = 0
 
 outer_loop:
-    CMP     X7, X6                  // Comparar k con n_iter
-    BGE     end_iterate             // Salir si k >= n_iter
-    MOV     X8, #0                  // i = 0
+    cmp     x7, x3                  // Comparar k con n_iter
+    bge     end_iterate             // Salir si k >= n_iter
+    mov     x8, #0                  // i = 0
 
 row_loop:
-    CMP     X8, X0                  // Comparar i con N
-    BGE     next_iteration          // Salir si i >= N
-    MOV     X9, #0                  // j = 0
+    cmp     x8, x0                  // Comparar i con N
+    bge     next_iteration          // Salir si i >= N
+    mov     x9, #0                  // j = 0
 
 column_loop:
-    CMP     X9, X0                  // Comparar j con N
-    BGE     next_row                // Salir si j >= N
-    MUL     X10, X8, X0             // i * N
-    ADD     X10, X10, X9            // i * N + j
-    MUL     X11, X8, X0             // fc_x * N
-    ADD     X11, X11, X9            // fc_x * N + fc_y
-    CMP     X10, X11                // Comparar (i*N + j) con (fc_x*N + fc_y)
-    BEQ     skip_cell               // Saltar si son iguales
+    cmp     x9, x0                  // Comparar j con N
+    bge     next_row                // Salir si j >= N
+    mul     x10, x8, x0             // i * N
+    add     x10, x10, x9            // i * N + j
+    mul     x11, x16, x0            // fc_x * N
+    add     x11, x11, x17           // fc_x * N + fc_y
+    cmp     x10, x11                // Comparar (i*N + j) con (fc_x*N + fc_y)
+    beq     skip_cell               // Saltar si son iguales
 
     // Calcular la suma de los vecinos
-    FMOV    D2, D0                  // sum = 0
-    ADD     X12, X8, #1             // i + 1
-    CMP     X12, X0                 // Comparar i + 1 con N
-    BGE     add_t_amb1              // Si i + 1 >= N, usar t_amb
-    MUL     X13, X12, X0            // (i + 1) * N
-    ADD     X13, X13, X9            // (i + 1) * N + j
-    LDR     D3, [X2, X13, LSL #3]   // Cargar x[(i + 1)*N + j]
-    B       add_sum1
+    fmov    d2, d0                  // sum = 0
+    add     x12, x8, #1             // i + 1
+    cmp     x12, x0                 // Comparar i + 1 con N
+    bge     add_t_amb1              // Si i + 1 >= N, usar t_amb
+    mul     x13, x12, x0            // (i + 1) * N
+    add     x13, x13, x9            // (i + 1) * N + j
+    ldr     d3, [x1, x13, lsl #3]   // Cargar x[(i + 1)*N + j]
+    b       add_sum1
 
 add_t_amb1:
-    LDR     D3, [X1]                // Cargar t_amb en D3
+    ldr     d3, [x4]                // Cargar t_amb en d3
 
 add_sum1:
-    FADD    D2, D2, D3              // sum = sum + x[(i + 1)*N + j] o t_amb
+    fadd    d2, d2, d3              // sum = sum + x[(i + 1)*N + j] o t_amb
 
-    SUB     X12, X8, #1             // i - 1
-    CMP     X12, #0                 // Comparar i - 1 con 0
-    BLT     add_t_amb2              // Si i - 1 < 0, usar t_amb
-    MUL     X13, X12, X0            // (i - 1) * N
-    ADD     X13, X13, X9            // (i - 1) * N + j
-    LDR     D3, [X2, X13, LSL #3]   // Cargar x[(i - 1)*N + j]
-    B       add_sum2
+    sub     x12, x8, #1             // i - 1
+    cmp     x12, #0                 // Comparar i - 1 con 0
+    blt     add_t_amb2              // Si i - 1 < 0, usar t_amb
+    mul     x13, x12, x0            // (i - 1) * N
+    add     x13, x13, x9            // (i - 1) * N + j
+    ldr     d3, [x1, x13, lsl #3]   // Cargar x[(i - 1)*N + j]
+    b       add_sum2
 
 add_t_amb2:
-    LDR     D3, [X1]                // Cargar t_amb en D3
+    ldr     d3, [x4]                // Cargar t_amb en d3
 
 add_sum2:
-    FADD    D2, D2, D3              // sum = sum + x[(i - 1)*N + j] o t_amb
+    fadd    d2, d2, d3              // sum = sum + x[(i - 1)*N + j] o t_amb
 
-    ADD     X12, X9, #1             // j + 1
-    CMP     X12, X0                 // Comparar j + 1 con N
-    BGE     add_t_amb3              // Si j + 1 >= N, usar t_amb
-    MUL     X13, X8, X0             // i * N
-    ADD     X13, X13, X12           // i * N + (j + 1)
-    LDR     D3, [X2, X13, LSL #3]   // Cargar x[i*N + j + 1]
-    B       add_sum3
+    add     x12, x9, #1             // j + 1
+    cmp     x12, x0                 // Comparar j + 1 con N
+    bge     add_t_amb3              // Si j + 1 >= N, usar t_amb
+    mul     x13, x8, x0             // i * N
+    add     x13, x13, x12           // i * N + (j + 1)
+    ldr     d3, [x1, x13, lsl #3]   // Cargar x[i*N + j + 1]
+    b       add_sum3
 
 add_t_amb3:
-    LDR     D3, [X1]                // Cargar t_amb en D3
+    ldr     d3, [x4]                // Cargar t_amb en d3
 
 add_sum3:
-    FADD    D2, D2, D3              // sum = sum + x[i*N + j + 1] o t_amb
+    fadd    d2, d2, d3              // sum = sum + x[i*N + j + 1] o t_amb
 
-    SUB     X12, X9, #1             // j - 1
-    CMP     X12, #0                 // Comparar j - 1 con 0
-    BLT     add_t_amb4              // Si j - 1 < 0, usar t_amb
-    MUL     X13, X8, X0             // i * N
-    ADD     X13, X13, X12           // i * N + (j - 1)
-    LDR     D3, [X2, X13, LSL #3]   // Cargar x[i*N + j - 1]
-    B       add_sum4
+    sub     x12, x9, #1             // j - 1
+    cmp     x12, #0                 // Comparar j - 1 con 0
+    blt     add_t_amb4              // Si j - 1 < 0, usar t_amb
+    mul     x13, x8, x0             // i * N
+    add     x13, x13, x12           // i * N + (j - 1)
+    ldr     d3, [x1, x13, lsl #3]   // Cargar x[i*N + j - 1]
+    b       add_sum4
 
 add_t_amb4:
-    LDR     D3, [X1]                // Cargar t_amb en D3
+    ldr     d3, [x4]                // Cargar t_amb en d3
 
 add_sum4:
-    FADD    D2, D2, D3              // sum = sum + x[i*N + j - 1] o t_amb
+    fadd    d2, d2, d3              // sum = sum + x[i*N + j - 1] o t_amb
 
     // Dividir la suma por 4 y almacenar en x_tmp
-    LDR     X14, =x_tmp             // Dirección de x_tmp
-    LDR     X15, =four              // Dirección de la constante 4.0
-    LDR     D4, [X15]               // Cargar 4.0 en D4
-    FDIV    D2, D2, D4              // sum / 4
-    STR     D2, [X14, X10, LSL #3]  // Almacenar sum / 4 en x_tmp[i*N + j]
+    ldr     x14, =x_tmp             // Dirección de x_tmp
+    ldr     x15, =four              // Dirección de la constante 4.0
+    ldr     d4, [x15]               // Cargar 4.0 en d4
+    fdiv    d2, d2, d4              // sum / 4
+    str     d2, [x14, x10, lsl #3]  // Almacenar sum / 4 en x_tmp[i*N + j]
 
 skip_cell:
-    ADD     X9, X9, #1              // Incrementar j
-    B       column_loop             // Repetir para la siguiente columna
+    add     x9, x9, #1              // Incrementar j
+    b       column_loop             // Repetir para la siguiente columna
 
 next_row:
-    ADD     X8, X8, #1              // Incrementar i
-    B       row_loop                // Repetir para la siguiente fila
+    add     x8, x8, #1              // Incrementar i
+    b       row_loop                // Repetir para la siguiente fila
 
 next_iteration:
-    LDR     X2, =x                  // Dirección de x
-    LDR     X14, =x_tmp             // Dirección de x_tmp
-    MOV     X15, #0                 // i = 0
+    ldr     x1, =x                  // Dirección de x
+    ldr     x14, =x_tmp             // Dirección de x_tmp
+    mov     x15, #0                 // i = 0
 
 copy_loop:
-    CMP     X15, X4                 // Comparar i con N*N
-    BGE     end_copy                // Salir si i >= N*N
-    MUL     X10, X8, X0             // fc_x * N
-    ADD     X10, X10, X9            // fc_x * N + fc_y
-    CMP     X15, X10                // Comparar i con fc_x*N + fc_y
-    BEQ     skip_copy               // Saltar si son iguales
-    LDR     D2, [X14, X15, LSL #3]  // Cargar x_tmp[i]
-    STR     D2, [X2, X15, LSL #3]   // Almacenar en x[i]
+    cmp     x15, x6                 // Comparar i con N*N
+    bge     end_copy                // Salir si i >= N*N
+    mul     x10, x8, x0             // fc_x * N
+    add     x10, x10, x9            // fc_x * N + fc_y
+    cmp     x15, x10                // Comparar i con fc_x*N + fc_y
+    beq     skip_copy               // Saltar si son iguales
+    ldr     d2, [x14, x15, lsl #3]  // Cargar x_tmp[i]
+    str     d2, [x1, x15, lsl #3]   // Almacenar en x[i]
 
 skip_copy:
-    ADD     X15, X15, #1            // Incrementar i
-    B       copy_loop               // Repetir
+    add     x15, x15, #1            // Incrementar i
+    b       copy_loop               // Repetir
 
 end_copy:
-    ADD     X7, X7, #1              // Incrementar k
-    B       outer_loop              // Repetir para la siguiente iteración
+    add     x7, x7, #1              // Incrementar k
+    b       outer_loop              // Repetir para la siguiente iteración
 
 end_iterate:
     // Fin del programa
-    MOV     X0, #0                  // Código de salida 0
-    MOV     X8, #93                 // syscall: exit
-    SVC     #0
-	
+    mov     x0, #0                  // Código de salida 0
+    mov     x8, #93                 // syscall: exit
+    svc     #0

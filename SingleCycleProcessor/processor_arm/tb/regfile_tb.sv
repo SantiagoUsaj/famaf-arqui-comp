b0c83da93a491294a7569df2f55f6f7212c73313
Santiago <santiusaj11@gmail.com>
2025-09-03 18:03:29 -0300

    ej 5 - regfile , tb missing

---
+++ SingleCycleProcessor/processor_arm/tb/regfile_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.09.2025 17:57:06
+// Design Name: 
+// Module Name: regfile_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module regfile_tb(
+
+    );
+endmodule

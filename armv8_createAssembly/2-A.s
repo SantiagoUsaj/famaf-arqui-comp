71229968319c677b9bfa021a4fe330c9843d28bb
Santiago <santiusaj11@gmail.com>
2025-09-06 18:21:14 -0300

    2-A fix and 2-B

--- armv8_createAssembly/2-A.s
+++ armv8_createAssembly/2-A.s
@@ -1,26 +1,29 @@
 	.text
 	.org 0x0000
 	
-	ADD x2, xzr, xzr 	// x2 = 0
+	ADD x2, x0, x0 	// x2 = 0
+	ADD x3, x0, x0 	// x3 = 0
 loop:					// loop:
-	SUB x3, x30, x0		// x3 = x30 - x0
-	CBZ x3, end			// if x3 == 0, goto end
-	STUR x0, [x2, #0]	// [x2] = x0
-	ADD x0, x1, x0		// x0 = x1 + x0
+	CBZ x30, end		// if x30 == 0, goto end
+	STUR x3, [x2, #0]	// [x2] = x3
+	ADD x3, x3, x1		// x3 = x3 + x1
+	SUB x30, x30, x1	// x30 = x30 - x1
 	ADD x2, x2, x8		// x2 = x2 + x8
-	CBZ xzr, loop		// if xzr == 0, goto loop
+	CBZ x0, loop		// if x0 == 0, goto loop
 end:
 
-infloop:
-	CBZ xzr, infloop
+infloop:	
+	CBZ x0, infloop
 
 /* 
-ROM [0:7] ='{32'h8b1f03e2,
-32'hcb1e0003,
-32'hb40000a3,
-32'hf8000040,
-32'h8b000020,
+N = 29
+ROM [0:8] ='{32'h8b000002,
+32'h8b000003,
+32'hb40000de,
+32'hf8000043,
+32'h8b010063,
+32'hcb0103de,
 32'h8b080042,
-32'hb4ffff7f,
-32'hb400001f}; 
+32'hb4ffff60,
+32'hb4000000};
 */
--- armv8_createAssembly/2-B.s
+++ armv8_createAssembly/2-B.s
@@ -1,16 +1,52 @@
 	.text
-	.org 0x0000
-	
-	ADD x2, xzr, xzr 	// x2 = 0
-loop:					// loop:
-	SUB x3, x0, x30		// x3 = x0 - x30
-	CBZ x3, end			// if x3 == 0, goto end
-	STUR x0, [x2, #0]	// [x2] = x0
-	ADD x0, x1, x0		// x0 = x1 + x0
+    .org 0x0000
+
+	ADD x2, x0, x0 	// x2 = 0
+	ADD x3, x0, x0 	// x3 = 0
+loop1:					// loop1:
+	CBZ x30, end1		// if x30 == 0, goto end1
+	STUR x3, [x2, #0]	// [x2] = x3
+	ADD x3, x3, x1		// x3 = x3 + x1
+	SUB x30, x30, x1	// x30 = x30 - x1
 	ADD x2, x2, x8		// x2 = x2 + x8
-	CBZ xzr, loop		// if xzr == 0, goto loop
-end:
+	CBZ x0, loop1		// if x0 == 0, goto loop1
+end1:
+
+	ADD x2, x0, x0
+	ADD x3, x0, x0
+	ADD x4, x0, x0
+loop2:
+	CBZ x9, end2		
+	LDUR x3, [x2, #0]
+	ADD x4, x4, x3
+	SUB x9, x9, x1
+	ADD x2, x2, x8
+	CBZ x0, loop2
+end2:
+	STUR x4, [x2, #0]
 
 infloop:
-	CBZ xzr, infloop
+	CBZ x0, infloop
 
+/* 
+N = 8
+ROM [0:18] ='{32'h8b000002,
+32'h8b000003,
+32'hb40000de,
+32'hf8000043,
+32'h8b010063,
+32'hcb0103de,
+32'h8b080042,
+32'hb4ffff60,
+32'h8b000002,
+32'h8b000003,
+32'h8b000004,
+32'hb40000c9,
+32'hf8400043,
+32'h8b030084,
+32'h8b080042,
+32'hcb010129,
+32'hb4ffff60,
+32'hf8000044,
+32'hb4000000};
+*/

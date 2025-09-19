0344f273068304b9de9a10380dfc04e9b18244be
Santiago <santiusaj11@gmail.com>
2025-09-19 11:32:22 -0300

    add flopr

---
+++ processor_arm/src/flopr.sv
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.08.2025 15:24:23
+// Design Name: 
+// Module Name: flopr
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module flopr 
+    #(parameter N = 64) 
+    (
+        input logic clk,
+        input logic reset,
+        input logic [N-1:0] d,
+        output logic [N-1:0] q
+    );
+
+    always_ff @(posedge clk, posedge reset)
+        if (reset)
+            q <= '0;
+        else
+            q <= d;    
+
+endmodule

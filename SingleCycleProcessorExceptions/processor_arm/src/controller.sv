03e1ee0d605dd0ca607e538eb83012ebf7b409f8
Santiago <santiusaj11@gmail.com>
2025-09-24 15:58:54 -0300

    tp3 - ej1 (no test)

--- SingleCycleProcessorExceptions/processor_arm/src/controller.sv
+++ SingleCycleProcessorExceptions/processor_arm/src/controller.sv
@@ -1,25 +1,48 @@
 // CONTROLLER
 
-module controller(input logic [10:0] instr,
-						output logic [3:0] AluControl,						
-						output logic reg2loc, regWrite, AluSrc, Branch,
-											memtoReg, memRead, memWrite);
+	module controller(
+	    input logic [10:0] instr,
+	    input logic reset,
+	    input logic ExtIRQ,
+	    input logic ExcAck,
+	    output logic [3:0] AluControl,
+	    output logic reg2loc, regWrite, Branch,
+	    output logic memtoReg, memRead, memWrite,
+	    output logic ExtIAck,
+	    output logic ERet,
+	    output logic Exc,
+	    output logic [3:0] EStatus,
+	    output logic [1:0] AluSrc // nuevo, salida de 2 bits
+	);
 											
-	logic [1:0] AluOp_s;
+		logic [1:0] AluOp_s,;
 											
-	maindec 	decPpal 	(.Op(instr), 
-							.Reg2Loc(reg2loc), 
-							.ALUSrc(AluSrc), 
-							.MemtoReg(memtoReg), 
-							.RegWrite(regWrite), 
-							.MemRead(memRead), 
-							.MemWrite(memWrite), 
-							.Branch(Branch), 
-							.ALUOp(AluOp_s));	
-					
-								
-	aludec 	decAlu 	(.funct(instr), 
-							.aluop(AluOp_s), 
-							.alucontrol(AluControl));
-			
+		maindec decPpal (
+			.Op(instr),
+			.reset(reset),
+			.ExtIRQ(ExtIRQ),
+			.Reg2Loc(reg2loc),
+			.MemtoReg(memtoReg),
+			.RegWrite(regWrite),
+			.MemRead(memRead),
+			.MemWrite(memWrite),
+			.Branch(Branch),
+			.ALUOp(AluOp_s),
+			.ALUSrc(AluSrc),
+			.Exc(Exc),
+			.ERet(ERet),
+			.EStatus(EStatus)
+		);
+		
+
+		aludec decAlu (
+			.funct(instr),
+			.aluop(AluOp_s),
+			.alucontrol(AluControl)
+		);
+
+		// ExtIAck: ‘1’ cuando ExcAck = ‘1’ y ExtIRQ = ‘1’, caso contrario ‘0’
+		assign ExtIAck = (ExcAck & ExtIRQ) & ~reset;
+
 endmodule
+
--- SingleCycleProcessorExceptions/processor_arm/src/maindec.sv
+++ SingleCycleProcessorExceptions/processor_arm/src/maindec.sv
@@ -22,85 +22,193 @@
 
 module maindec (
         input logic [10:0] Op,
+        input logic reset, // new
+        input logic ExtIRQ, // new
         output logic Reg2Loc,
-        output logic ALUSrc,
         output logic MemtoReg,
         output logic RegWrite,
         output logic MemRead,
         output logic MemWrite,
         output logic Branch,
-        output logic [1:0] ALUOp
+        output logic [1:0] ALUOp, ALUSrc, // update ALUSrc to 2 bits
+        output logic Exc, ERet, // new
+        output logic [3:0] EStatus // new
     );
-	
-	always_comb begin
-        casez(Op)
-            11'b111_1100__0010:  //LDUR CASE
+
+    logic NotAnInstr;
+    logic [3:0] Status;
+
+    always_comb begin
+        if (reset) begin
+            Reg2Loc = 0;
+            ALUSrc = 'b00;
+            MemtoReg = 0;
+            RegWrite = 0;
+            MemRead = 0;
+            MemWrite = 0;
+            Branch = 0;
+            ALUOp = 'b00;
+            NotAnInstr = 0;
+            Exc = 0;
+            EStatus = 4'b0000;
+            Status = 4'b0000;
+            ERet = 0;
+        end 
+        else begin
+            logic Reg2Loc_next, MemtoReg_next, RegWrite_next, MemRead_next, MemWrite_next, Branch_next, ERet_next;
+            logic [1:0] ALUOp_next, ALUSrc_next;
+            logic NotAnInstr_next;
+            logic [3:0] Status_next;
+
+
+            // Default values
+            Reg2Loc_next = 0;
+            ALUSrc_next = 'b00;
+            MemtoReg_next = 0;
+            RegWrite_next = 0;
+            MemRead_next = 0;
+            MemWrite_next = 0;
+            Branch_next = 0;
+            ALUOp_next = 'b00;
+            NotAnInstr_next = 0;
+            Status_next = 4'b0000;
+            ERet_next = 0;
+
+            casez(Op)
+                11'b111_1100__0010:  //LDUR CASE
                         begin
-                            Reg2Loc = 0;
-                            ALUSrc = 1;
-                            MemtoReg = 1;
-                            RegWrite = 1;
-                            MemRead = 1;
-                            MemWrite = 0;
-                            Branch = 0;
-                            ALUOp = 'b00;
+                            Reg2Loc_next = 'x;
+                            ALUSrc_next = 'b01;
+                            MemtoReg_next = 1;
+                            RegWrite_next = 1;
+                            MemRead_next = 1;
+                            MemWrite_next = 0;
+                            Branch_next = 0;
+                            ALUOp_next = 'b00;
+                            NotAnInstr_next = 0;
+                            Status_next = 4'b0000;
+                            ERet_next = 0;
                         end
-            11'b111_1100_0000: //STUR CASE
+                11'b111_1100_0000: //STUR CASE
                         begin
-                            Reg2Loc = 1;
-                            ALUSrc = 1;
-                            MemtoReg = 0;
-                            RegWrite = 0;
-                            MemRead = 0;
-                            MemWrite = 1;
-                            Branch = 0;
-                            ALUOp = 'b00;
+                            Reg2Loc_next = 1;
+                            ALUSrc_next = 'b01;
+                            MemtoReg_next = 'x;
+                            RegWrite_next = 0;
+                            MemRead_next = 0;
+                            MemWrite_next = 1;
+                            Branch_next = 0;
+                            ALUOp_next = 'b00;
+                            NotAnInstr_next = 0;
+                            Status_next = 4'b0000;
+                            ERet_next = 0;
                         end
-            11'b101_1010_0???: //CBZ CASE
+                11'b101_1010_0???: //CBZ CASE
                         begin
-                            Reg2Loc = 1;
-                            ALUSrc = 0;
-                            MemtoReg = 0;
-                            RegWrite = 0;
-                            MemRead = 0;
-                            MemWrite = 0;
-                            Branch = 1;
-                            ALUOp = 'b01;
+                            Reg2Loc_next = 1;
+                            ALUSrc_next = 'b00;
+                            MemtoReg_next = 'x;
+                            RegWrite_next = 0;
+                            MemRead_next = 0;
+                            MemWrite_next = 0;
+                            Branch_next = 1;
+                            ALUOp_next = 'b01;
+                            NotAnInstr_next = 0;
+                            Status_next = 4'b0000;
+                            ERet_next = 0;
                         end
-            11'b1?0_0101_1000: //ADD and SUB CASE
+                11'b1?0_0101_1000: //ADD and SUB CASE
                         begin
-                            Reg2Loc = 0;
-                            ALUSrc = 0;
-                            MemtoReg = 0;
-                            RegWrite = 1;
-                            MemRead = 0;
-                            MemWrite = 0;
-                            Branch = 0;
-                            ALUOp = 'b10;
+                            Reg2Loc_next = 0;
+                            ALUSrc_next = 'b00;
+                            MemtoReg_next = 0;
+                            RegWrite_next = 1;
+                            MemRead_next = 0;
+                            MemWrite_next = 0;
+                            Branch_next = 0;
+                            ALUOp_next = 'b10;
+                            NotAnInstr_next = 0;
+                            Status_next = 4'b0000;
+                            ERet_next = 0;
                         end
-            11'b10?_0101_0000: //AND and ORR CASE
+                11'b10?_0101_0000: //AND and ORR CASE
                         begin
-                            Reg2Loc = 0;
-                            ALUSrc = 0;
-                            MemtoReg = 0;
-                            RegWrite = 1;
-                            MemRead = 0;
-                            MemWrite = 0;
-                            Branch = 0;
-                            ALUOp = 'b10;
+                            Reg2Loc_next = 0;
+                            ALUSrc_next = 'b00;
+                            MemtoReg_next = 0;
+                            RegWrite_next = 1;
+                            MemRead_next = 0;
+                            MemWrite_next = 0;
+                            Branch_next = 0;
+                            ALUOp_next = 'b10;
+                            NotAnInstr_next = 0;
+                            Status_next = 4'b0000;
+                            ERet_next = 0;
                         end
-            default 
+                11'b110_1011_0100: // ERET CASE
                         begin
-                            Reg2Loc = 0;
-                            ALUSrc = 0;
-                            MemtoReg = 0;
-                            RegWrite = 0;
-                            MemRead = 0;
-                            MemWrite = 0;
-                            Branch = 0;
-                            ALUOp = 'b00;
+                            Reg2Loc_next = 0;
+                            ALUSrc_next = 'b00;
+                            MemtoReg_next = 'x;
+                            RegWrite_next = 0;
+                            MemRead_next = 0;
+                            MemWrite_next = 0;
+                            Branch_next = 1;
+                            ALUOp_next = 'b01;
+                            NotAnInstr_next = 0;
+                            Status_next = 4'b0000;
+                            ERet_next = 1;
                         end
-							
-        endcase
-    end
+                11'b110_1010_1001: // MRS CASE
+                        begin
+                            Reg2Loc_next = 1;
+                            ALUSrc_next = 'b1x;
+                            MemtoReg_next = 0;
+                            RegWrite_next = 1;
+                            MemRead_next = 0;
+                            MemWrite_next = 0;
+                            Branch_next = 0;
+                            ALUOp_next = 'b01;
+                            NotAnInstr_next = 0;
+                            Status_next = 4'b0000;
+                            ERet_next = 0;
+                        end
+                default // Invalid OpCode
+                        begin
+                            Reg2Loc_next = 'x;
+                            ALUSrc_next = 'bxx;
+                            MemtoReg_next = 0;
+                            RegWrite_next = 0;
+                            MemRead_next = 0;
+                            MemWrite_next = 0;
+                            Branch_next = 0;
+                            ALUOp_next = 'bxx;
+                            NotAnInstr_next = 1;
+                            Status_next = 4'b0010;
+                            ERet_next = 0;
+                        end
+            endcase
+
+            // Si hay IRQ externa, no modificar señales de control
+            if (ExtIRQ) begin
+                EStatus = 4'b0001;
+                // Las señales de control NO se modifican, se mantienen como estaban
+                // (no se actualizan en este ciclo)
+            end else begin
+                Reg2Loc = Reg2Loc_next;
+                ALUSrc = ALUSrc_next;
+                MemtoReg = MemtoReg_next;
+                RegWrite = RegWrite_next;
+                MemRead = MemRead_next;
+                MemWrite = MemWrite_next;
+                Branch = Branch_next;
+                ALUOp = ALUOp_next;
+                NotAnInstr = NotAnInstr_next;
+                Status = Status_next;
+                EStatus = Status_next;
+                ERet = ERet_next;
+            end
+            Exc = ((ExtIRQ | NotAnInstr_next) & ~reset);
+        end
+    end //always_comb    
 endmodule

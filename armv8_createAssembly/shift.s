6eb2097922fb10523e87ea2fc9792d2b97e26e96
Santiago <santiusaj11@gmail.com>
2025-11-06 12:35:46 -0300

    expansion idea

--- armv8_createAssembly/shift.s
+++ armv8_createAssembly/shift.s
@@ -1,3 +1,26 @@
 
-LSR X6, X11, #2 
-LSL X7, X12, #4  
+LSL X0, X0, #15
+
+LSL X1, X1, #15
+
+LSL X5, X5, #2 
+
+
+LSL X3, X3, #1
+
+LSL X3, X3, #7
+
+LSL X3, X3, #1
+
+LSL X3, X3, #7
+
+LSL X7, X3, #1      
+
+LSR X8, X3, #1     
+
+LSL X6, X6, #15  
+
+LSL X3, X3, #1
+
+LSL X3, X3, #7
+
